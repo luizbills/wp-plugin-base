e0281ac89ae8274b71e6aee3f707a598b52ceec7
Luiz "Bills <luizpbills@gmail.com>
2022-05-24 10:20:21 -0300

    Update Debug.php

--- core/Debug.php
+++ core/Debug.php
@@ -10,7 +10,7 @@ abstract class Debug {
 			if ( \is_callable( $message ) ) {
 				$message = $message();
 			}
-			if ( ! $exception_class || ! \class_exists( $exception_class ) ) {
+			if ( ! $exception_class ) {
 				$exception_class = \RuntimeException::class;
 			}
 			throw new $exception_class( $message, (int) $error_code );
@@ -21,25 +21,25 @@ abstract class Debug {
 		if ( ! defined( 'WP_DEBUG_LOG' ) || ! WP_DEBUG_LOG ) return;
 		$output = [];
 		foreach ( \func_get_args() as $arg ) {
-			$value = '';
-			if ( \is_object( $arg ) || \is_array( $arg ) ) {
-				$value = \print_r( $arg, true );
-			}
-			elseif ( \is_bool( $arg ) ) {
-				$value = $arg ? 'bool(true)' : 'bool(false)';
-			}
-			elseif ( '' === $arg ) {
-				$value = 'empty_string';
-			}
-			elseif ( null === $arg ) {
-				$value = 'NULL';
-			}
-			else {
-				$value = $arg;
-			}
-			$output[] = $value;
+			$output[] = self::format( $arg );
 		}
 		$slug = Config::get( 'SLUG' );
 		\error_log( "[$slug] " . \implode( ' ', $output ) );
+	}
+
+	public static function format ( $value ) {
+		if ( \is_object( $value ) || \is_array( $value ) ) {
+			$value = \print_r( $value, true );
+		}
+		elseif ( \is_bool( $arg ) ) {
+			$value = $value ? 'BOOL(TRUE)' : 'BOOL(FALSE)';
+		}
+		elseif ( '' === $value ) {
+			$value = 'EMPTY_STRING';
+		}
+		elseif ( null === $value ) {
+			$value = 'NULL';
+		}
+		return $value;
 	}
 }
