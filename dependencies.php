3746d41bef5c055df5bf2d747a58e6c833ca075b
Luiz Bills <luizbills@gmail.com>
2022-08-08 20:07:32 -0300

    update dependencies

--- dependencies.php
+++ dependencies.php
@@ -7,6 +7,11 @@ defined( 'WPINC' ) || exit( 1 );
 return [
 	// Check the PHP version
 	'php' => [
+		'check' => function () {
+			$req_version = h::config_get( 'REQUIRED_PHP_VERSION', false );
+			$serv_version = \preg_replace( '/[^0-9\.]/', '', PHP_VERSION );
+			return $req_version && $serv_version ? \version_compare( $serv_version, $req_version, '>=' ) : true;
+		},
 		'message' => function () {
 			$req_version = h::config_get( 'REQUIRED_PHP_VERSION', false );
 			return sprintf(
@@ -14,25 +19,20 @@ return [
 				__( "Update your %s version to $req_version or later.", 'your_text_domain' ),
 				'<strong>PHP</strong>'
 			);
-		},
-		'check' => function () {
-			$req_version = h::config_get( 'REQUIRED_PHP_VERSION', false );
-			$serv_version = \preg_replace( '/[^0-9\.]/', '', PHP_VERSION );
-			return $req_version && $serv_version ? \version_compare( $serv_version, $req_version, '>=' ) : true;
 		}
 	],
 
 	/*
 	// Check if WooCommerce is activated
 	'woocommerce' => [
+		'check' => function () {
+			return \function_exists( 'WC' );
+		},
 		'message' => sprintf(
 			// translators: %s is replaced with a required plugin name
 			__( 'Install and activate the %s plugin.', 'your_text_domain' ),
 			'<strong>WooCommerce</strong>'
-		),
-		'check' => function () {
-			return \function_exists( 'WC' );
-		}
+		)
 	],
 	*/
 ];
