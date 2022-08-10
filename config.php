f1fba4d1a52f37607321e755dd047fb452e50c8b
Luiz "Bills <luizpbills@gmail.com>
2022-08-10 09:00:10 -0300

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,5 @@ defined( 'WPINC' ) || exit( 1 );
 return [
 	'SLUG' => 'your_plugin_slug',
 	'PREFIX' => 'your_plugin_prefix',
-	'LANGUAGES_DIR' => 'languages',
-	'TEMPLATES_DIR' => 'templates'
+	'TEMPLATES_DIR' => 'templates',
 ];
