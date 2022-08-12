81699f04a06734cc1a1d84fac37afb8268c031a9
Luiz Bills <luizbills@gmail.com>
2022-08-12 10:42:11 -0300

    improve check_dependencies and add shortcuts

--- core/Dependencies.php
+++ core/Dependencies.php
@@ -7,57 +7,108 @@ use Your_Namespace\Core\Loader;
 
 abstract class Dependencies {
 	protected static $dependencies;
+	protected static $initialized = false;
 
-	public static function init ( $main_file ) {
-		if ( '' !== Config::get( 'PLUGIN_STARTED', '' ) ) {
+	public static function init () {
+		if ( self::$initialized ) {
 			throw new \Error( __CLASS__ . ' already initialized' );
 		}
 
 		$root = Config::get( 'DIR' );
 		self::$dependencies = include_once $root . '/dependencies.php';
-
 		if ( ! is_array( self::$dependencies ) ) {
 			throw new \Error( $root . '/dependencies.php must return an Array' );
 		}
 
-		\add_action( 'plugins_loaded', [ __CLASS__, 'check_dependencies' ], 0 );
+		\add_action( 'plugins_loaded', [ __CLASS__, 'maybe_start_plugin' ], 0 );
+
+		self::$initialized = true;
+	}
+
+	public static function maybe_start_plugin () {
+		$result = self::check_dependencies();
+
+		//var_dump( $result ); die;
+
+		if ( $result['success'] ) {
+			do_action( Loader::get_hook_start_plugin() );
+		} else {
+			self::display_notice_missing_deps( $result['messages'] );
+		}
 	}
 
 	public static function check_dependencies () {
-		$errors = [];
-		$passed = [];
+		$result = [
+			'success' => null,
+			'messages' => [],
+		];
+		$errors = 0;
 
-		foreach ( self::$dependencies as $dep ) {
-			$check = is_callable( $dep['check'] ) ? call_user_func( $dep['check'] ) : $dep['check'];
-			$message = is_callable( $dep['message'] ) ? call_user_func( $dep['message'] ) : $dep['message'];
+		foreach ( self::$dependencies as $key => $dep ) {
+			$check = $dep['check'] ?? null;
+			$message = $dep['message'] ?? null;
+			$message = is_callable( $message ) ? call_user_func( $message ) : $message;
 
-			if ( ! $check ) {
-				$errors[] = $message;
-			} else {
-				$passed[] = $message;
+			// check the message
+			if ( ! is_string( $message ) || '' === trim( $message ) ) {
+				$id = is_integer( $key ) ? '#' . ( 1 + $key ) : $key;
+				throw new \Error( "Dependency $id has an invalid 'message': its must be a string and and it cannot be empty." );
 			}
+
+			// check the requirement
+			$found = false;
+			if ( is_string( $check ) ) {
+				$found = self::handle_shortcut( $check );
+			} elseif ( is_callable( $check ) ) {
+				$found = call_user_func( $check );
+			} else {
+				$found = $check;
+			}
+
+			$result['messages'][] = [
+				'text' => $message,
+				'is_error' => ! boolval( $found ),
+			];
+
+			if ( ! boolval( $found ) ) $errors++;
 		}
 
-		self::$dependencies = null;
+		$result['success'] = ( 0 === $errors );
 
-		if ( count( $errors ) > 0 ) {
-			self::display_errors( $errors, $passed );
-		}
-
-		$should_start = 0 === count( $errors );
-
-		if ( $should_start ) {
-			Loader::start();
-		}
-
-		Config::set( 'PLUGIN_STARTED', $should_start );
+		return $result;
 	}
 
-	protected static function display_errors ( $errors, $passed ) {
-		if ( ! \is_admin() ) return;
+	protected static function handle_shortcut ( $string ) {
+		$parts = explode( ':', $string );
+		$value = implode( ':', array_slice( $parts, 1 ) );
+		switch ( $parts[0] ) {
+			case 'class':
+				return class_exists( $value );
+			case 'function':
+				return function_exists( $value );
+			case 'plugin':
+				if ( ! function_exists( 'is_plugin_active' ) ) {
+					include_once \ABSPATH . 'wp-admin/includes/plugin.php';
+				}
+				return \is_plugin_active( $value );
+			case 'module': // alias for 'extension'
+			case 'extension':
+				return extension_loaded( $value );
+			case 'const': // alias for 'defined'
+			case 'defined':
+				return defined( $value );
+			case 'wp': // alias for 'wordpress'
+			case 'wordpress':
+				return version_compare( get_bloginfo( 'version' ), $value, '>=' );
+		}
+	}
 
-		\add_action( 'admin_notices', function () use ( $errors, $passed ) {
-			if ( ! \current_user_can( 'install_plugins' ) ) return;
+	protected static function display_notice_missing_deps ( $messages ) {
+		if ( ! \is_admin() ) return;
+		if ( ! \current_user_can( 'install_plugins' ) ) return;
+		if ( 0 === ( $messages ) ) return;
+
+		\add_action( 'admin_notices', function () use ( $messages ) {
 			$allowed_html = [
 				'a' => [ 'href' => [], 'title' => [] ],
 				'span' => [ 'class' => [], 'style' => [] ],
@@ -73,23 +124,19 @@ abstract class Dependencies {
 				"<strong>" . Config::get( 'NAME' ) . "</strong>",
 			);
 
-			foreach ( $errors as $error_message ) {
+			foreach ( $messages as $message ) {
+				$icon = $message['is_error'] ? 'no-alt' : 'yes';
+				$color = $message['is_error'] ? '#e03131' : '#2b8a3e';
 				$line = \sprintf(
-					'<br>%s <span style="color:#e03131"><span class="dashicons dashicons-no-alt"></span> %s</span>',
+					'<br>%s<span style="color:%s"><span class="dashicons dashicons-%s"> </span>%s</span>',
 					\str_repeat( '&nbsp;', 4 ),
-					$error_message
+					$color,
+					$icon,
+					$message['text']
 				);
 				echo \wp_kses( $line, $allowed_html );
 			}
 
-			foreach ( $passed as $error_message ) {
-				$line = \sprintf(
-					'<br>%s <span style="color:#2b8a3e"><span class="dashicons dashicons-yes"></span> %s</span>',
-					\str_repeat( '&nbsp;', 4 ),
-					$error_message
-				);
-				echo \wp_kses( $line, $allowed_html );
-			}
 			echo '</p></div>';
 		} );
 	}
