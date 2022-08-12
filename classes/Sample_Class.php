c0d1b7230ca480285709bdbc229d0190131db9a6
Luiz Bills <luizbills@gmail.com>
2022-08-12 10:41:20 -0300

    auto run __activation and __deactivation methods

--- classes/Sample_Class.php
+++ classes/Sample_Class.php
@@ -7,9 +7,9 @@ use Your_Namespace\Helpers as h;
 final class Sample_Class {
 	public function __start () {
 		// This method is called automatically when all plugin dependencies are active.
-		// Use this method to add your actions and filters hooks
+		// But, you need put this class in your /loader.php first
 
-		// example:
+		// example: Use this method to add your actions and filters hooks
 		\add_action( 'plugins_loaded', [ $this, 'init' ], 10 );
 	}
 
@@ -17,5 +17,17 @@ final class Sample_Class {
 		$plugin_name = h::config_get( 'NAME' );
 		$version = h::get_plugin_version();
 		h::log( "INFO Running plugin \"$plugin_name\" v$version"  );
+	}
+
+	public static function __activation () {
+		// This STATIC method is called automatically on plugin activation.
+		$plugin_name = h::config_get( 'NAME' );
+		h::log( "INFO plugin \"$plugin_name\" activated"  );
+	}
+
+	public static function __deactivation () {
+		// This STATIC method is called automatically on plugin deactivation (not uninstall/deletation).
+		$plugin_name = h::config_get( 'NAME' );
+		h::log( "INFO plugin \"$plugin_name\" deactivated"  );
 	}
 }
--- loader.php
+++ loader.php
@@ -6,10 +6,6 @@ use Your_Namespace\Helpers as h;
 
 defined( 'WPINC' ) || exit( 1 );
 
-// register_activation_hook( h::config_get( 'FILE' ), function () {
-// 	h::log( 'plugin activated' );
-// } );
-
 return [
 	[ Sample_Class::class, 10 ], // 10 is priority
 ];
